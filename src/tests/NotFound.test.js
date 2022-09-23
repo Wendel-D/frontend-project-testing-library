f5f504779e5d4fc839bcfe08387f3012fd898569
wendel-D <wendeldaniel263@gmail.com>
2022-09-23 10:16:24 -0300

    requisito 4 ok

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,24 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import NotFound from '../pages/NotFound';
+
+describe('testando NotFound.js', () => {
+  it('Teste se a página contém um heading h2', () => {
+    renderWithRouter(<NotFound />);
+    const h2 = screen.getByRole('heading', {
+      level: 2, name: /Page requested not found/i,
+    });
+    expect(h2).toBeInTheDocument();
+  });
+
+  it('Teste se a página mostra a imagem pikachu chorando', () => {
+    renderWithRouter(<NotFound />);
+    const imgUrl = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    const img = screen.getByRole('img', {
+      name: /pikachu crying because the page requested was not found/i,
+    });
+    expect(img).toBeInTheDocument();
+    expect(img).toHaveAttribute('src', imgUrl);
+  });
+});
